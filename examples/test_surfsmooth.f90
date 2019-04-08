b8e19d5abbaac253229f18d74cc6b54ab08403a5
Mike ONeil <oneil@cims.nyu.edu>
2019-04-08 12:24:49 -0400

    added some more geometires

--- examples/test_surfsmooth.f90
+++ examples/test_surfsmooth.f90
@@ -66,7 +66,7 @@ program smoother
   !
   ! specify the msh file to read in
   !
-  nombre='./geometries/substraction/substraction2.msh'
+  nombre='./geometries/mi_barco_simple.msh'
   filename='./plot_files/high_genus'
   ! point inside to check Gauss integral
   x0 = 0
